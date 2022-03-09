7dc3afe30eeb9c8048c47c8dc8eb2cb4518797e7
t819856 <t819856@VW10A26958.principalusa.corp.principal.com>
2022-03-09 12:22:51 +0530

    New project with demo class created.

---
+++ MyPro/src/Demo.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Demo {
+	public static void main(String args[])
+	{
+		List mylist = new ArrayList();
+		mylist.add(100);
+		mylist.add("hello");
+		mylist.add(3.4);
+		System.out.println(mylist);
+	}
+
+}
